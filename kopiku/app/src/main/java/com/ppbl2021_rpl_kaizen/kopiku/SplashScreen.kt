af413aa38e6fb80e34011266cfad83618e75dc12
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2021-04-17 18:58:18 +0700

    Tambah Logic Ke Splash Screen, Agar Muncul Landing Page

--- kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/SplashScreen.kt
+++ kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/SplashScreen.kt
@@ -1,11 +1,21 @@
 package com.ppbl2021_rpl_kaizen.kopiku
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val i = Intent(this, LandingScreen::class.java)
+            startActivity(i);
+            finish()
+        }, 2000)
     }
+
 }
