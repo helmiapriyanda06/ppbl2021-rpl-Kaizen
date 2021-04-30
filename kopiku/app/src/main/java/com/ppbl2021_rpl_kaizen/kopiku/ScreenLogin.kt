6ee2496f18a09044a9629b5b5d50477d7514b782
helmiapriyanda <56975427+helmiapriyanda@users.noreply.github.com>
2021-04-30 13:18:10 +0700

    login

--- kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/ScreenLogin.kt
+++ kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/ScreenLogin.kt
@@ -1,4 +1,37 @@
 package com.ppbl2021_rpl_kaizen.kopiku
 
-class ScreenLogin {
-}
+import android.app.ProgressDialog
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class ScreenLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+
+        val login
+        login.setOnClickListener {
+            val inputEmail
+            val email = inputEmail.text.toString()
+            val inputPassword
+            val password = inputPassword.text.toString()
+            if (email.isEmpty()|| password.isEmpty()) {
+                Toast.makeText(this, "Please Insert Email and Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(email == "admin01@gmail.com" || password == "admin01"){
+                val progressDialog = ProgressDialog(this,
+                        R.style.Theme_MaterialComponents_Light_Dialog)
+                progressDialog.isIndeterminate = true
+                progressDialog.setMessage("Loading...")
+                progressDialog.show()
+                val intent = Intent (this,Dashboard::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        }
+    }
+
