dde95763c153862e812769baf219af3e6abbf938
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2021-04-30 16:18:38 +0700

    Menambahkan findViewById pada val login, inputemail, inputpassword

--- kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/ScreenLogin.kt
+++ kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/ScreenLogin.kt
@@ -3,19 +3,21 @@ package com.ppbl2021_rpl_kaizen.kopiku
 import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class ScreenLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
 
-        val login
+        val login = findViewById<Button>(R.id.pesan_now)
         login.setOnClickListener {
-            val inputEmail
+            val inputEmail = findViewById<TextInputEditText>(R.id.inputUsrname)
             val email = inputEmail.text.toString()
-            val inputPassword
+            val inputPassword = findViewById<TextInputEditText>(R.id.inputPassword)
             val password = inputPassword.text.toString()
             if (email.isEmpty()|| password.isEmpty()) {
                 Toast.makeText(this, "Please Insert Email and Password", Toast.LENGTH_SHORT).show()
