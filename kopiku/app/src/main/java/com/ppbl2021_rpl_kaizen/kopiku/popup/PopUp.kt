2a699fd2adfe374ac950a7a8f85ea11fe0526cc5
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2021-07-14 20:18:37 +0700

    Update PopUp.kt

--- kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/popup/PopUp.kt
+++ kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/popup/PopUp.kt
@@ -35,24 +35,6 @@ class PopUp : AppCompatActivity() {
         popupButton = bundle?.getString("popupbtn", "Button") ?: ""
         darkStatusBar = bundle?.getBoolean("darkstatusbar", false) ?: false
 
-        if (Build.VERSION.SDK_INT in 19..20) {
-            setWindowFlag(this, true)
-        }
-        if (Build.VERSION.SDK_INT >= 19) {
-            window.decorView.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                // If you want dark status bar, set darkStatusBar to true
-                if (darkStatusBar) {
-                    this.window.decorView.systemUiVisibility =
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                }
-                this.window.statusBarColor = Color.TRANSPARENT
-                setWindowFlag(this, false)
-            }
-        }
         val alpha = 100 //between 0-255
         val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
         val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), Color.TRANSPARENT, alphaColor)
@@ -61,15 +43,5 @@ class PopUp : AppCompatActivity() {
             CardViewPopUp.setBackgroundColor(animator.animatedValue as Int)
         }
         colorAnimation.start()
-    }
-    private fun setWindowFlag(activity: Activity, on: Boolean) {
-        val win = activity.window
-        val winParams = win.attributes
-        if (on) {
-            winParams.flags = winParams.flags or WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-        } else {
-            winParams.flags = winParams.flags and WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS.inv()
-        }
-        win.attributes = winParams
     }
 }
