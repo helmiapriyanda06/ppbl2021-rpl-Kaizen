3f8f9cb628af039149d9ea6be1aca7d7fdf8b8ad
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2021-07-14 20:13:11 +0700

    Coba coba pop up

---
+++ kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/popup/PopUp.kt
@@ -0,0 +1,75 @@
+package com.ppbl2021_rpl_kaizen.kopiku.popup
+
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.core.graphics.ColorUtils
+import com.ppbl2021_rpl_kaizen.kopiku.R
+import kotlinx.android.synthetic.main.activity_pop_up.*
+import kotlinx.android.synthetic.main.popup_main.*
+
+class PopUp : AppCompatActivity() {
+    private var popupTitle = ""
+    private var popupText = ""
+    private var popupButton = ""
+    private var darkStatusBar = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        overridePendingTransition(0, 0)
+        setContentView(R.layout.activity_pop_up)
+
+        titlepopup.text = popupTitle
+        detailpopup.text = popupText
+
+        val bundle = intent.extras
+        popupTitle = bundle?.getString("popuptitle", "Title") ?: ""
+        popupText = bundle?.getString("popuptext", "Text") ?: ""
+        popupButton = bundle?.getString("popupbtn", "Button") ?: ""
+        darkStatusBar = bundle?.getBoolean("darkstatusbar", false) ?: false
+
+        if (Build.VERSION.SDK_INT in 19..20) {
+            setWindowFlag(this, true)
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // If you want dark status bar, set darkStatusBar to true
+                if (darkStatusBar) {
+                    this.window.decorView.systemUiVisibility =
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                }
+                this.window.statusBarColor = Color.TRANSPARENT
+                setWindowFlag(this, false)
+            }
+        }
+        val alpha = 100 //between 0-255
+        val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
+        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), Color.TRANSPARENT, alphaColor)
+        colorAnimation.duration = 500 // milliseconds
+        colorAnimation.addUpdateListener { animator ->
+            CardViewPopUp.setBackgroundColor(animator.animatedValue as Int)
+        }
+        colorAnimation.start()
+    }
+    private fun setWindowFlag(activity: Activity, on: Boolean) {
+        val win = activity.window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+        } else {
+            winParams.flags = winParams.flags and WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS.inv()
+        }
+        win.attributes = winParams
+    }
+}
