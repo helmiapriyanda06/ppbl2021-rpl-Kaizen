1933208ba9b64aed5bdb6428cb2b661190fd6291
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2021-07-14 19:22:17 +0700

    nambah menu di main

--- kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/ui/MainActivity.kt
+++ kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/ui/MainActivity.kt
@@ -2,16 +2,43 @@ package com.ppbl2021_rpl_kaizen.kopiku.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.ppbl2021_rpl_kaizen.kopiku.Model.Coffee
 import com.ppbl2021_rpl_kaizen.kopiku.R
+import com.ppbl2021_rpl_kaizen.kopiku.adapter.AdminAdapter
+import com.ppbl2021_rpl_kaizen.kopiku.databinding.ActivityDashboardBinding
+import com.ppbl2021_rpl_kaizen.kopiku.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tvNoMeja : TextView
+    private lateinit var adapter: AdminAdapter
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.RVMakanan.layoutManager = LinearLayoutManager(this)
+        binding.RVMakanan.setHasFixedSize(true)
+        adapter = AdminAdapter(this)
+
+        firestore = Firebase.firestore
+        auth = Firebase.auth
 
         tvNoMeja = findViewById(R.id.user_name)
 
@@ -19,5 +46,34 @@ class MainActivity : AppCompatActivity() {
         {
             tvNoMeja.setText(intent.getStringExtra("data1"))
         }
+        loadQuotes()
+    }
+
+    private fun loadQuotes() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val quotesList = ArrayList<Coffee>()
+            firestore.collection("Coffee")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        val id = document.id
+                        val namaBarang = document.get("nama_barang").toString()
+                        val hargaBarang = document.get("harga_barang").toString()
+                        val kategori = document.get("kategori").toString()
+                        quotesList.add(Coffee(id, namaBarang , hargaBarang, kategori))
+                    }
+                    if (quotesList.size > 0) {
+                        binding.RVMakanan.adapter = adapter
+                        adapter.listQuotes = quotesList
+                    } else {
+                        adapter.listQuotes = ArrayList()
+                        showSnackbarMessage("Tidak ada data saat ini")
+                    }
+                }
+
+        }
+    }
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(binding.RVMakanan, message, Snackbar.LENGTH_SHORT).show()
     }
 }
