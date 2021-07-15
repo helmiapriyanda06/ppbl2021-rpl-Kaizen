a8687884c0acde7bc7a090d408801bda70878753
helmiapriyanda <56975427+helmiapriyanda@users.noreply.github.com>
2021-07-15 18:59:17 +0700

    cart

---
+++ kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/Model/Cart.kt
@@ -0,0 +1,5 @@
+package com.ppbl2021_rpl_kaizen.kopiku.Model
+
+data class Cart(
+    val coffees: ArrayList<ItemCart> = arrayListOf()
+)
---
+++ kopiku/app/src/main/java/com/ppbl2021_rpl_kaizen/kopiku/Model/ItemCart.kt
@@ -0,0 +1,6 @@
+package com.ppbl2021_rpl_kaizen.kopiku.Model
+
+data class ItemCart(
+    val coffee: Coffee,
+    var quantity: Int = 0
+)
